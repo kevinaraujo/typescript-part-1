3b5f40817acf77135ba784ac583a41c6c6b25de8
Kevin Araujo <kevinaraujo@kevinaraujo1.mac.hpicorp.net>
2022-04-27 18:21:00 -0300

    Added method to remove js script from html.

--- app/views/view.ts
+++ app/views/view.ts
@@ -1,14 +1,22 @@
 export abstract class View<T> {
     protected element: HTMLElement;
+    private escapar = false;
 
-    constructor(selector: string) {
+    constructor(selector: string, escapar?: boolean) {
         this.element = document.querySelector(selector);
+        if (escapar) {
+            this.escapar = escapar;
+        }
     }
 
     protected abstract template(model: T): string;
 
     public update(model: T): void {
-        const template = this.template(model);
+        let template = this.template(model);
+        if (this.escapar) {
+            template = template
+                .replace(/<script>[\s\S]*?<\/script>/, '')
+        }
 
         this.element.innerHTML = template;
     }
