f9c7222fb57a7b679b23830e0b6efcd155375b85
Kevin Araujo <kevinaraujo@kevinaraujo1.mac.hpicorp.net>
2022-05-04 14:59:29 -0300

    Applied decorator to check execution time.

---
+++ app/decorators/logar-tempo-de-execucao.ts
@@ -0,0 +1,21 @@
+export function logarTempoDeExecucao() {
+    return function (
+        target: any,
+        propertyKey: String,
+        descriptor: PropertyDescriptor
+    ) {
+        const metodoOriginal = descriptor.value;
+
+        descriptor.value = function (...args: Array<any>) {
+            const t1 = performance.now();
+            const retornoMetodoOriginal = metodoOriginal.apply(this, args);
+            const t2 = performance.now();
+
+            console.log(`${propertyKey}, tempo de execucao: ${(t2-t1)/1000} segundos.`);
+
+            return retornoMetodoOriginal;
+        }
+
+        return descriptor;
+    }
+}
