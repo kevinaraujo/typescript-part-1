2740b1573712dc20674963166d9a4df989e33276
Kevin Araujo <kevinaraujo@kevinaraujo1.mac.hpicorp.net>
2022-06-01 10:15:12 -0300

    feat: Added inspect decorator. Added condition into logar-tempo decorator.

--- app/decorators/logar-tempo-de-execucao.ts
+++ app/decorators/logar-tempo-de-execucao.ts
@@ -1,17 +1,25 @@
-export function logarTempoDeExecucao() {
+export function logarTempoDeExecucao(emSegundos: boolean = false) {
     return function (
         target: any,
         propertyKey: String,
         descriptor: PropertyDescriptor
     ) {
         const metodoOriginal = descriptor.value;
-
+        console.log(`descriptor`, descriptor);
         descriptor.value = function (...args: Array<any>) {
+            let divisor = 1;
+            let unidade = 'milisegundos';
+
+            if (emSegundos) {
+                divisor = 1000;
+                unidade = 'segundos';
+            }
+            console.log(`this`, this);
             const t1 = performance.now();
             const retornoMetodoOriginal = metodoOriginal.apply(this, args);
             const t2 = performance.now();
 
-            console.log(`${propertyKey}, tempo de execucao: ${(t2-t1)/1000} segundos.`);
+            console.log(`${propertyKey}, tempo de execucao: ${(t2-t1)/divisor} ${unidade}.`);
 
             return retornoMetodoOriginal;
         }
--- app/views/view.ts
+++ app/views/view.ts
@@ -1,3 +1,4 @@
+import { inspect } from "../decorators/inspect.js";
 import { logarTempoDeExecucao } from "../decorators/logar-tempo-de-execucao.js";
 
 export abstract class View<T> {
@@ -20,7 +21,8 @@ export abstract class View<T> {
 
     protected abstract template(model: T): string;
 
-    @logarTempoDeExecucao()
+    @logarTempoDeExecucao(true)
+    @inspect
     public update(model: T): void {
         let template = this.template(model);
         if (this.escapar) {
